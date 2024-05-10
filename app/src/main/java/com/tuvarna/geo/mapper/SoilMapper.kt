fa1b5c15a32d22fd62f1d291b7ac8742a1708960
Iliyan Kostov <iliyan.kostov@email.ikostov.org>
2024-05-10 23:34:05 +0300

    🚧 Feat: adding a mapping of soil datatype

---
+++ app/src/main/java/com/tuvarna/geo/mapper/SoilMapper.kt
@@ -0,0 +1,8 @@
+package com.tuvarna.geo.mapper
+
+class SoilMapper(val faosoi: String, val domsoi: String, val phases: String) {
+  //
+  //  fun readSoilConfig() {
+  //    val objectMapper =
+  //  }
+}
--- app/src/main/java/com/tuvarna/geo/view/component/home/SoilTable.kt
+++ app/src/main/java/com/tuvarna/geo/view/component/home/SoilTable.kt
@@ -20,24 +20,36 @@ import com.tuvarna.geo.view.component.table.TableKeyValueTextRow
 @Composable
 fun SoilTable(soil: Soil) {
   if (soil.gid != null) {
+
     Box(
       modifier =
         Modifier.padding(16.dp)
           .fillMaxWidth()
           .verticalScroll(rememberScrollState())
           .background(color = Color(151, 212, 168), shape = RoundedCornerShape(16.dp))
-          .border(BorderStroke(1.dp, Color.Black))
+          .border(BorderStroke(1.dp, Color(151, 212, 168)), shape = RoundedCornerShape(16.dp))
           .padding(8.dp)
     ) {
       Column(modifier = Modifier.fillMaxWidth()) {
-        TableKeyValueTextRow("Gid:", soil.gid.toString())
-        TableKeyValueTextRow("Snum:", soil.snum.toString())
-        TableKeyValueTextRow("Faosoil:", soil.faosoil ?: "None")
-        TableKeyValueTextRow("Domsoi:", soil.domsoi ?: "None")
-        TableKeyValueTextRow("Phases:", soil.phase1 + " " + soil.phase2)
-        TableKeyValueTextRow("Misclus:", soil.misclu1 + " " + soil.misclu2)
-        TableKeyValueTextRow("Country:", soil.country ?: "None")
-        TableKeyValueTextRow("SQ Kilometers:", soil.sqkm.toString())
+        // TableKeyValueTextRow("Gid:", soil.gid.toString())
+        TableKeyValueTextRow("Sequential code:", soil.snum.toString())
+        TableKeyValueTextRow("Dominant soil:", soil.faosoil ?: "None") // Bk
+        TableKeyValueTextRow("Texture class:", soil.faosoil ?: "None") // 2/3
+        TableKeyValueTextRow("Slope class:", soil.faosoil ?: "None") // ab
+
+        if (soil.phase1 != null || soil.phase2 != null)
+          TableKeyValueTextRow("Phases:", "${soil.phase1 ?: ""} ${soil.phase2 ?: ""}")
+        if (soil.misclu1 != null || soil.misclu2 != null)
+          TableKeyValueTextRow("Miscues:", "${soil.misclu1 ?: ""} ${soil.misclu2 ?: ""}")
+
+        if (soil.permafrost != null) {
+          TableKeyValueTextRow(
+            "Permafrost:",
+            if (soil.permafrost == "1") "Permafrost" else "Discontinuous permafrost",
+          )
+        }
+        TableKeyValueTextRow("Country:", soil.country?.lowercase()?.capitalize() ?: "None")
+        TableKeyValueTextRow("Square kilometers:", soil.sqkm.toString())
       }
     }
   }
